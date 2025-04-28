d6802268de2ef490aa916b151121b1e5b758810b
Blackninja5ll <simon.bandek@outlook.com>
2025-04-28 14:12:25 +0200

    test

--- app.js
+++ app.js
@@ -6,32 +6,37 @@ document.addEventListener("DOMContentLoaded", () => {
   loaded = true;
 });
 
-const checkWeather = () => {
+const startChecker = () => {
   if(loaded) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
-
+  
         console.log("Your location:", lat, lon);
-
-        // Step 2: Fetch weather for that location
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=se`;
-
-        fetch(url)
-          .then(response => response.json())
-          .then(data => {
-            console.log("Weather Data:", data);
-            console.log("Temperature:", data.main.temp + "°C");
-            console.log("Weather:", data.weather[0].description);
-          })
-          .catch(error => {
-            console.error("Error fetching weather data:", error);
-          });
+  
+        setInterval(() => {
+          checkWeather(lat, lon);
+        }, 5000);
       },
       (error) => {
         console.error("Error getting location:", error);
       }
     );
   }
+}
+
+const checkWeather = (lat, lon) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=se`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log("Weather Data:", data);
+      console.log("Temperature:", data.main.temp + "°C");
+      console.log("Weather:", data.weather[0].description);
+    })
+    .catch(error => {
+      console.error("Error fetching weather data:", error);
+    });
 }
